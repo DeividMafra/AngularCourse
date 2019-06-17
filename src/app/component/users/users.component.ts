991e5dfbb6ae3b5a56a59242e18034d230157a42
DeividMafra <deividmafra@aol.com>
2019-06-17 03:07:34 -0400

    Learning *ngFor and *ngIf

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    // console.log('init...');
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Deivid',
+          lastName: 'Mafra',
+          age: 33,
+          address: {
+            street: '1101 Bay St',
+            city: 'Toronto',
+            province: 'ON'
+          }
+        },
+        {
+          firstName: 'Mary',
+          lastName: 'Thompson',
+          age: 30,
+          address: {
+            street: '2702 Bloor St',
+            city: 'Toronto',
+            province: 'ON'
+          }
+        },
+        {
+          firstName: 'Laura',
+          lastName: 'Kawasaky',
+          age: 36,
+          address: {
+            street: '3803 Younge St',
+            city: 'Toronto',
+            province: 'ON'
+          }
+        },
+      ];
+      this.loaded = true;
+    }, 5000);
+
+
+    this.showExtended = false;
+
+    this.addUser({
+      firstName: 'Alexander',
+      lastName: 'Duck',
+      age: 28,
+      address: {
+        street: '3803 Younge St',
+        city: 'Toronto',
+        province: 'ON'
+      }
+    })
+
+  }
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
